4aec4ac5a7638ed2d92c56fbcc082d58490a0b91
llJONAKARYll <joelr.3214@gmail.com>
2024-11-21 21:49:47 -0300

    uwu

--- ev3segurito1/Controllers/RegistroController.cs
+++ ev3segurito1/Controllers/RegistroController.cs
@@ -1,6 +1,46 @@
-﻿namespace ev3segurito1.Controllers
-{
-    public class RegistroController
+﻿using Microsoft.AspNetCore.Mvc;
+using ev3segurito1.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using ev3segurito1.DataBase;
+
+ 
+    namespace ev3segurito1.Controllers{
+   
+
+    public class RegistroController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public RegistroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Index
+        public IActionResult Index()
+        {
+            var registros = _context.Registro.ToList();
+            return View(registros);
+        }
+
+        // GET: Crear
+        [HttpGet]
+        public IActionResult Crear() => View();
+
+        // POST: Crear
+        [HttpPost]
+        public IActionResult Crear(Registro registro)
+        {
+            if (ModelState.IsValid)
+            {
+                registro.FechaCreacion = DateTime.Now;
+                _context.Registro.Add(registro);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(registro);
+        }
     }
+
 }
