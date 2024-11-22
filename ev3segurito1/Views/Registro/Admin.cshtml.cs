aa71721eafb60b208baaca4bd9660fbbb649593c
llJONAKARYll <joelr.3214@gmail.com>
2024-11-21 22:29:54 -0300

    UnU

---
+++ ev3segurito1/Views/Registro/Admin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ev3segurito1.Views.Registro
+{
+    public class AdminModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ ev3segurito1/Views/Registro/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ev3segurito1.Views.Registro
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
